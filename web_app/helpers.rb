2c85de6f31713693a3cc8ff45f6509ff90d28dd7
Austin G. Davis-Richardson <harekrishna@gmail.com>
2013-06-27 15:15:53 -0400

    Time.at not Time.new

--- web_app/helpers.rb
+++ web_app/helpers.rb
@@ -5,7 +5,7 @@ def average_temperature(args={})
   since = args[:since]
   averaged = args[:averaged] # hour, day, minute, etc...
 
-  temps = Measurement.all(:created_at.gt => Time.new(since))
+  temps = Measurement.all(:created_at.gt => Time.at(since))
     .group_by { |x| x.created_at.send(averaged) }
     .map { |x| p [x[1].first.created_at, x[1].map(&:temperature).mean] }
 
